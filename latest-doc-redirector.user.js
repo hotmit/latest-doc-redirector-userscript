bf4ddeb4ae432b4ade055778e66942ce6df07e49
Du Dang <ddang@mesalabs.com>
2017-02-10 11:49:34 -0500

    removed jq

--- latest-doc-redirector.user.js
+++ latest-doc-redirector.user.js
@@ -20,55 +20,11 @@
     var PYTHON_TARGET_VERSION = '3';
     var DJANGO_TARGET_VERSION = '1.10';
     var RUBY_TARGET_VERSION = '2.4.0';
-	
-    var Str = {};
-
-    $(function(){
-		var href = window.location.href;
-
-		function forward(url){
-			var lastFwd = GM_getValue('LastUrlFwd', '');
-			// do this to allow browser to go back to old version 
-			// 		without loop redirect
-			if (lastFwd != url){
-				GM_setValue('LastUrlFwd', url);
-				window.location.href = url;
-			}
-		}
-
-		if (Str.contains(href, 'docs.djangoproject.com') 
-			&& !Str.contains(href, '/en/' + DJANGO_TARGET_VERSION + '/'))
-		{
-			// https://docs.djangoproject.com/en/1.5/topics/db/optimization/
-			// INTO https://docs.djangoproject.com/en/1.7/topics/db/optimization/
-			href = href.replace(/docs.djangoproject.com\/(\w{2})\/(\d\.\d|dev)/gi, 
-					'docs.djangoproject.com/en/' + DJANGO_TARGET_VERSION);
-			forward(href);
-		}
-
-		else if (Str.contains(href, 'docs.python.org') 
-			&& !Str.contains(href, '/' + PYTHON_TARGET_VERSION + '/'))
-		{
-			// https://docs.python.org/2/library/datetime.html
-			// INTO https://docs.python.org/3/library/datetime.html
-			href = href.replace(/docs.python.org\/2\/library/gi, 
-					'docs.python.org/' + PYTHON_TARGET_VERSION + '/library');
-			forward(href);
-		}
-
-		else if (Str.contains(href, 'ruby-doc.org/core-') 
-			&& !Str.contains(href, '/core-' + RUBY_TARGET_VERSION + '/'))
-		{
-			// https://ruby-doc.org/core-2.2.0/String.html
-			// INTO https://ruby-doc.org/core-2.3.1/String.html
-			href = href.replace(/core-[^\/]+/gi, 'core-' + RUBY_TARGET_VERSION);
-			forward(href);
-		}
-		
-    }); //end jqReady
 
     // region [ Str Lib ]
-	Str.contains = function(s, needle, caseSensitive) {        
+    var Str = {};
+
+    Str.contains = function(s, needle, caseSensitive) {
         if (caseSensitive){
             return s.indexOf(needle) > -1;
         }
@@ -76,4 +32,44 @@
     };
     // endregion
 
+    var href = window.location.href;
+
+    function forward(url){
+        var lastFwd = GM_getValue('LastUrlFwd', '');
+        // do this to allow browser to go back to old version
+        // 		without loop redirect
+        if (lastFwd != url){
+            GM_setValue('LastUrlFwd', url);
+            window.location.href = url;
+        }
+    }
+
+    if (Str.contains(href, 'docs.djangoproject.com') &&
+        !Str.contains(href, '/en/' + DJANGO_TARGET_VERSION + '/'))
+    {
+        // https://docs.djangoproject.com/en/1.5/topics/db/optimization/
+        // INTO https://docs.djangoproject.com/en/1.7/topics/db/optimization/
+        href = href.replace(/docs.djangoproject.com\/(\w{2})\/(\d\.\d|dev)/gi,
+                            'docs.djangoproject.com/en/' + DJANGO_TARGET_VERSION);
+        forward(href);
+    }
+
+    else if (Str.contains(href, 'docs.python.org') &&
+             !Str.contains(href, '/' + PYTHON_TARGET_VERSION + '/'))
+    {
+        // https://docs.python.org/2/library/datetime.html
+        // INTO https://docs.python.org/3/library/datetime.html
+        href = href.replace(/docs.python.org\/2\/library/gi,
+                            'docs.python.org/' + PYTHON_TARGET_VERSION + '/library');
+        forward(href);
+    }
+
+    else if (Str.contains(href, 'ruby-doc.org/core-') &&
+             !Str.contains(href, '/core-' + RUBY_TARGET_VERSION + '/'))
+    {
+        // https://ruby-doc.org/core-2.2.0/String.html
+        // INTO https://ruby-doc.org/core-2.3.1/String.html
+        href = href.replace(/core-[^\/]+/gi, 'core-' + RUBY_TARGET_VERSION);
+        forward(href);
+    }
 })();
